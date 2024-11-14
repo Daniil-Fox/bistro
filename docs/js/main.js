fbc7e3b4edabe894b91060f4829b5e74cbeb8e36
Artyushenko Daniil <foxones80@gmail.com>
2024-11-14 17:12:31 +0600

    added cart

--- docs/js/main.js
+++ docs/js/main.js
@@ -10528,6 +10528,37 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_components.js */ "./src/js/_components.js");
 
+const cart = document.querySelector('.cart');
+if (cart) {
+  const checkAll = document.querySelector('.cart__check input');
+  const checkItem = document.querySelectorAll('.cart__items input[type=checkbox]');
+  checkAll.addEventListener('change', e => {
+    if (checkAll.checked) {
+      checkItem.forEach(el => el.checked = true);
+    } else {
+      checkItem.forEach(el => el.checked = false);
+    }
+  });
+  checkItem.forEach(item => {
+    item.addEventListener('change', e => {
+      if (!item.checked) {
+        checkAll.checked = false;
+      }
+      if (allChecks()) {
+        checkAll.checked = true;
+      }
+    });
+  });
+  function allChecks() {
+    let flag = true;
+    checkItem.forEach(el => {
+      if (el.checked == false) {
+        flag = false;
+      }
+    });
+    return flag;
+  }
+}
 })();
 
 /******/ })()
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,38 @@
 import './_components.js';
+
+
+const cart = document.querySelector('.cart')
+
+if(cart){
+  const checkAll = document.querySelector('.cart__check input')
+  const checkItem = document.querySelectorAll('.cart__items input[type=checkbox]')
+  checkAll.addEventListener('change', e => {
+    if(checkAll.checked){
+      checkItem.forEach(el => el.checked = true)
+    } else {
+      checkItem.forEach(el => el.checked = false)
+    }
+  })
+
+  checkItem.forEach(item => {
+    item.addEventListener('change', e => {
+      if(!item.checked){
+        checkAll.checked = false
+      }
+
+      if(allChecks()){
+        checkAll.checked = true
+      }
+    })
+  })
+
+  function allChecks(){
+    let flag = true
+    checkItem.forEach(el => {
+      if(el.checked == false) {
+        flag = false
+      }
+    })
+    return flag
+  }
+}
